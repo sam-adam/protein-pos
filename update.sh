307fbcd3ac12254dda92ba9dc9c248e8e708c5db
Samuel <samuel.suhendra@gmail.com>
2016-12-20 10:18:47 +0700

    update update.sh

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ mysqldump -uroot --password=root proteinpos > proteinpos.sql
 
 scp -i /home/sam/Dropbox/Work/Misc/PEM/lightsail.pem proteinpos.sql ubuntu@54.147.84.243:~/proteinpos.sql
 
-ssh lightsail "mysql -uroot --password=root -e 'drop database proteinpos' && mysql -uroot --password=root -e 'create database proteinpos' && mysql -uroot --password=root proteinpos < ~/proteinpos.sql && rm ~/proteinpos.sql"
+ssh lightsail "cd proteinpos && git pull && mysql -uroot --password=root -e 'drop database proteinpos' && mysql -uroot --password=root -e 'create database proteinpos' && mysql -uroot --password=root proteinpos < ~/proteinpos.sql && rm ~/proteinpos.sql"
 
 rm proteinpos.sql
