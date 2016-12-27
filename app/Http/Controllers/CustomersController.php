4e9037d858c3a9482ce4a8befb430a50b24c398c
Samuel <samuel.suhendra@gmail.com>
2016-12-27 19:50:54 +0700

    customer edit

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -98,9 +98,37 @@ class CustomersController extends AuthenticatedController
         return redirect(route('customers.index'))->with('flashes.success', 'Customer added');
     }
 
-    public function edit() { }
+    public function edit($customerId)
+    {
+        $customer = Customer::find($customerId);
 
-    public function update() { }
+        if (!$customer) {
+            return redirect()->back()->with('flashes.error', 'Customer not found');
+        }
+
+        return view('customers.edit', [
+            'customer' => $customer,
+            'groups'   => CustomerGroup::all()
+        ]);
+    }
+
+    public function update(StoreCustomer $request, $customerId)
+    {
+        $customer = Customer::find($customerId);
+
+        if (!$customer) {
+            return redirect()->back()->with('flashes.error', 'Customer not found');
+        }
+
+        $customer->name                 = $request->get('name') ?: $customer->name;
+        $customer->phone                = $request->get('phone');
+        $customer->email                = $request->get('email');
+        $customer->address              = $request->get('address');
+        $customer->customer_group_id    = $request->get('customer_group_id') ?: null;
+        $customer->saveOrFail();
+
+        return redirect(Session::get('last_customer_page') ?: route('customers.index'))->with('flashes.success', 'Customer edited');
+    }
 
     public function destroy() { }
 }
--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title')
-    - Update Customer Group
+    - Update Customer
 @endsection
 
 @section('content')
@@ -10,26 +10,58 @@
         <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    Update Customer Group - {{ $group->name }}
+                    Update Customer - {{ $customer->name }}
                 </div>
                 <div class="panel-body">
-                    <form method="post" action="{{ route('customer-groups.update', $group->id) }}" class="form-horizontal">
+                    <form method="post" action="{{ route('customers.update', $customer->id) }}" class="form-horizontal">
                         {{ csrf_field() }}
                         {{ method_field('PUT') }}
                         <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
                             <label for="name" class="col-sm-2 control-label">Name</label>
                             <div class="col-sm-5">
-                                <input type="text" id="name" name="name" class="form-control" placeholder="Eg: VIP Group" required value="{{ old('name') ?: $group->name }}" />
+                                <input type="text" id="name" name="name" class="form-control" placeholder="Eg: John Doe" required value="{{ old('name') ?: $customer->name }}" />
                                 @foreach($errors->get('name') as $error)
                                     <span class="label label-danger">{{ $error }}</span>
                                 @endforeach
                             </div>
                         </div>
-                        <div class="form-group {{ $errors->has('discount') ? 'has-error' : '' }}">
-                            <label for="discount" class="col-sm-2 control-label">Discount</label>
+                        <div class="form-group {{ $errors->has('phone') ? 'has-error' : '' }}">
+                            <label for="phone" class="col-sm-2 control-label">Phone</label>
                             <div class="col-sm-5">
-                                <input type="text" id="discount" name="discount" class="form-control" placeholder="Between 1 - 100" required value="{{ old('discount') ?: $group->discount }}" />
-                                @foreach($errors->get('discount') as $error)
+                                <input type="text" id="phone" name="phone" class="form-control" placeholder="Eg: +1-881-556-0649" value="{{ old('phone') ?: $customer->phone }}" />
+                                @foreach($errors->get('phone') as $error)
+                                    <span class="label label-danger">{{ $error }}</span>
+                                @endforeach
+                            </div>
+                        </div>
+                        <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+                            <label for="email" class="col-sm-2 control-label">Email</label>
+                            <div class="col-sm-5">
+                                <input type="text" id="email" name="email" class="form-control" placeholder="Eg: john@doe.com" value="{{ old('email') ?: $customer->email }}" />
+                                @foreach($errors->get('email') as $error)
+                                    <span class="label label-danger">{{ $error }}</span>
+                                @endforeach
+                            </div>
+                        </div>
+                        <div class="form-group {{ $errors->has('address') ? 'has-error' : '' }}">
+                            <label for="address" class="col-sm-2 control-label">Address</label>
+                            <div class="col-sm-5">
+                                <textarea name="address" class="form-control" placeholder="Address">{{ old('address') ?: $customer->address }}</textarea>
+                                @foreach($errors->get('address') as $error)
+                                    <span class="label label-danger">{{ $error }}</span>
+                                @endforeach
+                            </div>
+                        </div>
+                        <div class="form-group {{ $errors->has('customer_group_id') ? 'has-error' : '' }}">
+                            <label for="customer-group-id" class="col-sm-2 control-label">Group</label>
+                            <div class="col-sm-5">
+                                <select id="customer-group-id" name="customer_group_id" class="form-control">
+                                    <option value>Select Customer Group</option>
+                                    @foreach($groups as $group)
+                                        <option value="{{ $group->id }}" @if((old('customer_group_id') ?: $customer->customer_group_id) == $group->id) selected @endif>{{ $group->name.' ('.$group->discount.'% discount)' }}</option>
+                                    @endforeach
+                                </select>
+                                @foreach($errors->get('customer_group_id') as $error)
                                     <span class="label label-danger">{{ $error }}</span>
                                 @endforeach
                             </div>
@@ -39,7 +71,7 @@
                                 <button type="submit" class="btn btn-success btn-block">Update</button>
                             </div>
                             <div class="col-sm-2">
-                                <a href="{{ URL::previous() === route('customer-groups.edit', $group->id) ? route('customer-groups.index') : URL::previous() }}" class="btn btn-danger btn-block">Cancel</a>
+                                <a href="{{ Session::get('last_customer_page') ?: route('customers.index') }}" class="btn btn-danger btn-block">Cancel</a>
                             </div>
                         </div>
                     </form>
