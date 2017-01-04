9421d97b1528594931b79d081c4de177f645be5f
Samuel <samuel.suhendra@gmail.com>
2017-01-04 22:42:20 +0700

    vue

--- resources/views/sales/create.blade.php
+++ resources/views/sales/create.blade.php
@@ -13,7 +13,7 @@
                     <div class="form-group form-group-lg">
                         <div class="input-group">
                             <div class="input-group-addon"><i class="fa fa-fw fa-search"></i></div>
-                            <input type="text" id="search-input" placeholder="Input product name, code, or scan barcode" class="form-control"/>
+                            <input type="text" id="query" placeholder="Input product name, code, or scan barcode" class="form-control" v-model="query" />
                         </div>
                     </div>
                 </div>
@@ -31,6 +31,16 @@
                                 <th>Total</th>
                             </tr>
                         </thead>
+                        <tbody>
+                            <tr v-for="cartItem in cart">
+                                <td></td>
+                                <td>@{{ cartItem.product.name }}</td>
+                                <td>@{{ cartItem.product.price }}</td>
+                                <td>@{{ cartItem.quantity }}</td>
+                                <td>@{{ 0 }}</td>
+                                <td>@{{ cartItem.product.price * cartItem.quantity }}</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
@@ -43,13 +53,55 @@
 @section('scripts')
     @parent
     <script type="text/javascript">
+        var $queryBox = $("#query");
+
+        $queryBox[0].focus();
+
         const app = new Vue({
             el: "#app",
+            data: {
+                query: "",
+                cart: []
+            },
             methods: {
-                findByBarcode: function(query) {
+                addProduct: function (product, quantity) {
+                    var sameProduct = false;
 
+                    this.cart.forEach(function (cartItem) {
+                        if (cartItem.product.id === product.id) {
+                            cartItem.quantity += quantity;
+                            sameProduct = true;
+                        }
+                    });
+
+                    if (!sameProduct) {
+                        this.cart.push({
+                            product: product,
+                            quantity: quantity
+                        })
+                    }
+                },
+                findByBarcode: function(query) {
+                    var $this = this;
+
+                    $.get("{{ url('/products/xhr-search') }}", {
+                        query: query
+                    }, function (response) {
+                        $this.addProduct(response.product, 1);
+                        $this.query = "";
+                    });
                 }
             }
+        });
+
+        $(document).ready(function () {
+            $(window).on("paste", function (e) {
+                if ($(e.target).attr("id") === $queryBox.attr("id")) {
+                    setTimeout(function () {
+                        app.findByBarcode($queryBox.val());
+                    }, 100);
+                }
+            });
         });
     </script>
 @endsection
