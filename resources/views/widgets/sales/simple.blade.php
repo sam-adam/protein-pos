85bbb524cc012b4ab1abe6d85903052691ee3477
Samuel <samuel.suhendra@gmail.com>
2017-02-22 09:29:48 +0700

    #116 - Add total

--- resources/views/widgets/sales/simple.blade.php
+++ resources/views/widgets/sales/simple.blade.php
@@ -2,6 +2,14 @@
     <div class="panel-heading">Today Sales Summary</div>
     <div class="panel-body">
         <div class="row">
+            <div class="col-sm-12">
+                <h4 class="text-center">Total</h4>
+                <h3 class="text-center">
+                    <span class="text-primary">@money($salesSummary['cash'] + $salesSummary['credit'])</span> AED
+                </h3>
+            </div>
+        </div>
+        <div class="row">
             <div class="col-sm-6">
                 <h4 class="text-center">Cash</h4>
                 <h3 class="text-center">
